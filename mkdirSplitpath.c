c64cf976aec3fed1e1dd2c6529e1a960619bacda
Chase Beckley <chase_beckley@mines.edu>
2025-03-30 22:17:49 -0600

    Completed mkdir and fixed changing of pathName in splitPath

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -10,7 +10,44 @@ void mkdir(char pathName[]){
     //
     // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
     
-    printf("TO BE IMPLEMENTED\n");
+    if (strcmp(pathName, "/") == 0) { // path is root alone, none specificed
+        printf("MKDIR ERROR: no path provided\n");
+	return;
+    }
+    char* baseName = malloc(strlen(pathName) + 1);
+    char* dirName = malloc(strlen(pathName) + 1);
+
+    struct NODE* parentNode = splitPath(pathName, baseName, dirName);
+
+    free(dirName); // dont need it
+    if (parentNode == NULL) return;
+
+    struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+    strcpy(newNode->name,baseName);
+    newNode->fileType = 'D';
+    newNode->parentPtr = parentNode;
+    newNode->childPtr = NULL;
+    newNode->siblingPtr = NULL;
+   
+    if (parentNode->childPtr != NULL) { // check if it has children to ensure none of them are the dir we are making
+        struct NODE* siblingNode = parentNode;
+        struct NODE* currentNode = parentNode->childPtr;
+
+        while (currentNode != NULL) { // go through each sibling
+	    if (strcmp(currentNode->name,baseName) == 0) { // if it has the same name, quit
+	        printf("MKDIR ERROR: directory %s already exists\n", pathName);
+	    	return;
+	    }
+	    siblingNode = currentNode;
+	    currentNode = currentNode->siblingPtr;
+	}
+
+	siblingNode->siblingPtr = newNode;
+    } else {
+    	parentNode->childPtr = newNode;
+    }
+
+    printf("MKDIR SUCCESS: node %s successfully created\n", pathName);
 
     return;
 }
@@ -35,30 +72,33 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
 	return cwd;
     }
 
-    char* last_slash = strrchr(pathName, '/');
+    char *tempPathName = malloc(strlen(pathName) + 1);
+    strcpy(tempPathName, pathName);
+
+    char* last_slash = strrchr(tempPathName, '/');
 	
     *last_slash = '\0'; // split that string into two
 
-    strcpy(dirName, pathName);
+    strcpy(dirName, tempPathName);
     strcpy(baseName, last_slash + 1);
 
     struct NODE* currentNode; // node that will go down the tree
     
-    if (pathName[0] == '/') {
+    if (tempPathName[0] == '/') {
         currentNode = root;
-        pathName = pathName + 1; // advance pathname forward so the root indicator doesnt screw with delimiter
+        tempPathName = tempPathName + 1; // advance pathname forward so the root indicator doesnt screw with delimiter
     } else currentNode = cwd; // set it to either absolute or relative
 
-    char *token = strtok(pathName, "/");
+    char *token = strtok(tempPathName, "/");
 
     while (token != NULL) { // loop over every delimited directory name
 	if (currentNode->childPtr == NULL) {
 	    printf("ERROR: directory %s does not exist\n", token);
 	    return NULL; // quit if there is more to search for but dir is empty
 	}
-        
+      	
 	currentNode = currentNode->childPtr;
-        while (strcmp(currentNode->name,"") != 0) { // if the name of the current node doesn't match
+        while (strcmp(currentNode->name,token) != 0) { // if the name of the current node doesn't match
 	    if (currentNode->siblingPtr == NULL) {
 	        printf("ERROR: directory %s does not exist\n", token);
 	        return NULL; // quit if there is no directories left to search
