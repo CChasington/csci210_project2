16ea2d75ccbfc93fc4a2abba27fa6f7842a8ee0b
Chase Beckley <chase_beckley@mines.edu>
2025-03-30 21:01:02 -0600

    mkdirSplitPath complete

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -9,7 +9,7 @@ void mkdir(char pathName[]){
     // TO BE IMPLEMENTED
     //
     // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
-    printf("HELLO WORLD");
+    
     printf("TO BE IMPLEMENTED\n");
 
     return;
@@ -23,7 +23,54 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
     // rm, rmdir, ls, cd, touch COMMANDS WILL NOT EXECUTE CORRECTLY
     // SEE THE PROVIDED SOLUTION EXECUTABLE TO SEE THEIR EXPECTED BEHAVIOR
 
-    // YOUR CODE HERE
-    //
-    return NULL;
+    if (strcmp(pathName, "/") == 0) { // if path is root alone
+	strcpy(dirName, "/");
+	strcpy(baseName, "");
+	return root;
+    }
+
+    if (strchr(pathName, '/') == NULL) { // if path is relative file name alone
+        strcpy(dirName, "");
+	strcpy(baseName, pathName);
+	return cwd;
+    }
+
+    char* last_slash = strrchr(pathName, '/');
+	
+    *last_slash = '\0'; // split that string into two
+
+    strcpy(dirName, pathName);
+    strcpy(baseName, last_slash + 1);
+
+    struct NODE* currentNode; // node that will go down the tree
+    
+    if (pathName[0] == '/') {
+        currentNode = root;
+        pathName = pathName + 1; // advance pathname forward so the root indicator doesnt screw with delimiter
+    } else currentNode = cwd; // set it to either absolute or relative
+
+    char *token = strtok(pathName, "/");
+
+    while (token != NULL) { // loop over every delimited directory name
+	if (currentNode->childPtr == NULL) {
+	    printf("ERROR: directory %s does not exist\n", token);
+	    return NULL; // quit if there is more to search for but dir is empty
+	}
+        
+	currentNode = currentNode->childPtr;
+        while (strcmp(currentNode->name,"") != 0) { // if the name of the current node doesn't match
+	    if (currentNode->siblingPtr == NULL) {
+	        printf("ERROR: directory %s does not exist\n", token);
+	        return NULL; // quit if there is no directories left to search
+	    }
+
+	    currentNode = currentNode->siblingPtr; // check the next sibling
+	}
+
+	// reaching this point means a child directory with the name was located. currentNode cannot be null
+
+        token = strtok(NULL, "/");
+    } 
+
+    return currentNode;
 }
